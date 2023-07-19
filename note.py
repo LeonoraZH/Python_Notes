38a31e37bc7e9f0e0a2e4ffcf13de589dbdd4353
Leonora Zharova <norka_200@mail.rugit config --global user.namegit config --global user.email norka_200@mail.ru>
2023-07-19 16:21:31 +0300

    ability of adding notes was added

---
+++ note.py
@@ -0,0 +1,65 @@
+import json
+import datetime
+import os
+
+NOTES_FILE = "notes.json"
+
+
+def load_notes():
+    if not os.path.exists(NOTES_FILE):
+        return []
+    with open(NOTES_FILE, "r") as file:
+        return json.load(file)
+
+
+def save_notes(notes):
+    with open(NOTES_FILE, "w") as file:
+        json.dump(notes, file, indent=4)
+
+
+def create_note():
+    title = input("Введите заголовок заметки: ")
+    body = input("Введите тело заметки: ")
+    created_at = datetime.datetime.now().isoformat()
+    return {
+        "id": len(notes) + 1,
+        "title": title,
+        "body": body,
+        "created_at": created_at,
+        "last_modified": created_at
+    }
+
+
+def add_note():
+    new_note = create_note()
+    notes.append(new_note)
+    save_notes(notes)
+    print("Заметка успешно сохранена.")
+
+
+def main():
+    global notes
+    notes = load_notes()
+
+    while True:
+        print("\nВведите команду:")
+        print("add - добавить новую заметку")
+        print("list - посмотреть список заметок")
+        print("edit - редактировать заметку")
+        print("delete - удалить заметку")
+        print("filter - фильтровать заметки по дате")
+        print("exit - выход")
+
+        command = input().lower()
+
+        if command == "add":
+            add_note()
+        elif command == "exit":
+            print("Выход из программы.")
+            break
+        else:
+            print("Некорректная команда. Попробуйте снова.")
+
+
+if __name__ == "__main__":
+    main()
